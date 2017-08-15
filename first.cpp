0c72f123996e8eb75b9521348d45a1ae34f83e6f
rikkitikkitumbo <rikkitikkitumbo@gmail.com>
2017-08-15 16:29:41 -0700

    messing with gdb

--- first.cpp
+++ first.cpp
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <string>
 
 int main()
 {
-  std::string name;
-  name = "greg";
-  printf("%s is a great name!",name.c_str());
+	std::string name = "greg";
+	int age = 31;
+  printf("\n\n %s is a great name!\n",name.c_str());
+  printf("but %d is getting old!\n\n", age);
 }
 
